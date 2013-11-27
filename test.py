08be4e92399142be1352cb18e3788f7451db5a6d
Francisco Gárate <pyriskorg@gmail.com>
2013-11-28 00:15:59 +0100

    introducir moon

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ print range(1,9)
 for i in range(1,10):
     print i
     
-repls = {'hello' : 'goodbye', 'world' : 'earth'}
 s = 'hello, world'
 print reduce(lambda a, kv: a.replace(*kv), repls.iteritems(), s)
 
